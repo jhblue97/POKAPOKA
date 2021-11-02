dbf12443cbc9cac16fb73d7f364a3fb37ea8e8e3
OrnateValor <paeksh94@gmail.com>
2021-11-02 19:59:09 +0900

    21.11.02 파일명 수정 psh

--- src/main/java/com/poka/controller/GameController.java
+++ src/main/java/com/poka/controller/GameController.java
@@ -23,19 +23,21 @@ public class GameController {
 
 	@GetMapping("/list")
 	// 게임 목록 - 메인화면
-	public void list(Criteria cri, Model model) {
+	public String list(Criteria cri, Model model) {
 		log.info("메인 - 게임 목록 출력"); // 실행 확인용 로그
 		model.addAttribute("list", gService.getList(cri));
 		// 무한 스크롤 페이징?
-
+		return "/game/main.jsp";
 	}
 
 	@GetMapping("/get/{gno}")
 	// 게임 상세 화면
 	// gno 받아와서 MODEL에 담아줄 것.
-	public void get(@PathVariable("gno") String gno, @ModelAttribute("cri") Criteria cri, Model model) {
+	public String get(@PathVariable("gno") String gno, @ModelAttribute("cri") Criteria cri, Model model) {
 		log.info("게임 상세 페이지 출력"); // 실행 확인용 로그
 		model.addAttribute("game", gService.get(gno));
+
+		return "/game/gameView.jsp";
 	}
 
 }
