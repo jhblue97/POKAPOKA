92e8b8941ed9c4f4429b456957d9c6dc3680d287
juhyeon <@DESKTOP-C84PAR8>
2021-11-02 12:03:15 +0900

    20211102-낮 합본 NIJI CONTROLLER 수정 -JH

--- src/main/java/com/poka/controller/NijiController.java
+++ src/main/java/com/poka/controller/NijiController.java
@@ -1,5 +1,14 @@
 package com.poka.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,10 +17,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 import com.poka.domain.Criteria;
+import com.poka.domain.NijiAttachVO;
 import com.poka.domain.NijiVO;
+import com.poka.domain.PayAttachVO;
+import com.poka.service.NijiService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -23,6 +37,8 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class NijiController {
 
+	private NijiService nijiService;
+	
 	     //게시물 목록 조회
 		@GetMapping("/list")
 		public String list(Criteria cri, Model model) {			
@@ -33,7 +49,7 @@ public class NijiController {
 		
 		 //게시물 검색 조회
 		@GetMapping("/search")
-		public String list(@RequestParam("search") String search,Criteria cri, Model model) {			
+		public String list(@RequestParam("name") String name,Criteria cri, Model model) {			
 			log.info(".....list().....");
 			return null;
 		}		
@@ -66,6 +82,9 @@ public class NijiController {
 		
 		
 		
+		
+		
+		
 		//게시물 삭제
 		@GetMapping("/delete")
 		@PreAuthorize("principal.username == #writer") //작성자 확인
@@ -76,6 +95,77 @@ public class NijiController {
 			return null;
 		}
 		
+		
+		//첨부파일 삭제
+		public void deleteFile(NijiAttachVO attach) {
+			log.info(".....deleteFiles().....");
+			if(attach == null) {	//첨부파일이 없는 경우 중단
+				return;
+			}
+			
+			try {
+				Path file = Paths.get("c:\\upload\\" + attach.getUploadPath() + "\\" +
+													   attach.getUuid() + "_" +
+													   attach.getFileName());
+				Files.deleteIfExists(file);	//파일이 존재하면 삭제
+				
+				if(Files.probeContentType(file).startsWith("image")) {	//이미지 파일의 경우 섬네일 삭제
+					Path thumbnail = Paths.get("c:\\upload\\" + attach.getUploadPath() + "\\s_" +
+													   			attach.getUuid() + "_" +
+													   			attach.getFileName());
+					Files.delete(thumbnail);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			}
+			
+			
+			//첨부파일 삭제
+			public void deleteFile(PayAttachVO attach) {
+				log.info(".....deleteFiles().....");
+				if(attach == null) {	//첨부파일이 없는 경우 중단
+					return;
+				}
+				
+				try {
+					Path file = Paths.get("c:\\upload\\" + attach.getUploadPath() + "\\" +
+														   attach.getUuid() + "_" +
+														   attach.getFileName());
+					Files.deleteIfExists(file);	//파일이 존재하면 삭제
+					
+					if(Files.probeContentType(file).startsWith("image")) {	//이미지 파일의 경우 섬네일 삭제
+						Path thumbnail = Paths.get("c:\\upload\\" + attach.getUploadPath() + "\\s_" +
+														   			attach.getUuid() + "_" +
+														   			attach.getFileName());
+						Files.delete(thumbnail);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+				
+				
+				
+		//첨부파일 목록 JSON 반환
+		@GetMapping(value="/displayFile",
+				    produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+		@ResponseBody
+		public ResponseEntity<NijiAttachVO> getNijiAttach(String nno){
+			log.info(".....getAttachList().....");
+			return new ResponseEntity<>(nijiService.getAttach(nno), HttpStatus.OK);
+		}
+		
+		//첨부파일 목록 JSON 반환
+				@GetMapping(value="/pay/displayFile",
+						    produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+				@ResponseBody
+				public ResponseEntity<NijiAttachVO> getPayAttach(String pno){
+					log.info(".....getAttachList().....");
+					return new ResponseEntity<>(nijiService.getAttach(pno), HttpStatus.OK);
+				}
+				
+				
 		//게시물 수정
 		@GetMapping("/modify")
 		@PreAuthorize("principal.username == #board.writer") //작성자 확인
@@ -139,6 +229,7 @@ public class NijiController {
 				log.info(".....get() or modify() .....");			
 				return null;	
 		}
+		
 		
 		
 		
