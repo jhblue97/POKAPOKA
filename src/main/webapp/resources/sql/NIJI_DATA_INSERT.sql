8dfcf7427be2ac25718ebd80decd47dd21be6fc5
juhyeon <@DESKTOP-C84PAR8>
2021-11-04 11:46:09 +0900

    20211104 - NIJI 검색수정+NIJI테스트데이터SQL추가 -JH

---
+++ src/main/webapp/resources/sql/NIJI_DATA_INSERT.sql
@@ -0,0 +1,4 @@
+Insert into DEV.P_NIJI (NNO,TITLE,CONTENT,CATEGORY,IMG,PRICE,WRITER,REGDATE,UPDATEDATE) values ('N10','제목7','내용7','S','default.png',1500,'user',to_date('21/11/03','RR/MM/DD'),null);
+Insert into DEV.P_NIJI (NNO,TITLE,CONTENT,CATEGORY,IMG,PRICE,WRITER,REGDATE,UPDATEDATE) values ('N11','test','test','S','default.png',1500,'user',to_date('21/11/03','RR/MM/DD'),null);
+Insert into DEV.P_NIJI (NNO,TITLE,CONTENT,CATEGORY,IMG,PRICE,WRITER,REGDATE,UPDATEDATE) values ('N12','제목10','내용10','S','default.png',1500,'user',to_date('21/11/03','RR/MM/DD'),null);
+Insert into DEV.P_NIJI (NNO,TITLE,CONTENT,CATEGORY,IMG,PRICE,WRITER,REGDATE,UPDATEDATE) values ('N13','제목11','내용11','B','default.png',1500,'user',to_date('21/11/03','RR/MM/DD'),null);
