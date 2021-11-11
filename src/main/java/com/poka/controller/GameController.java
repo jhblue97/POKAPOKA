428a12381be3eb15867352a96185f3a3fd5a0c13
OrnateValor <paeksh94@gmail.com>
2021-11-11 18:35:51 +0900

    21.11.11 Steam API connection PSH

--- src/main/java/com/poka/controller/GameController.java
+++ src/main/java/com/poka/controller/GameController.java
@@ -3,7 +3,6 @@ package com.poka.controller;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.springframework.stereotype.Controller;
